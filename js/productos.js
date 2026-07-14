8d70714ed01468f06e6c4ddeccae316b1a7ba14b
AbiLemus <abigailemus@gmail.com>
2026-07-14 18:57:05 +1200

    agrego productos, y carrito

---
+++ js/productos.js
@@ -0,0 +1,31 @@
+console.log("productos.js conectado");
+const productos = [
+  {
+    id: 1,
+    nombre: "Mochila viajera",
+    precio: 150000,
+    imagen: "img producto/mochila.jpg",
+    categoria: "equipaje"
+  },
+  {
+    id: 2,
+    nombre: "Bitácora",
+    precio: 50000,
+    imagen: "img producto/bitacora.jpg",
+    categoria: "viajes"
+  },
+  {
+    id: 3,
+    nombre: "Organizador de maleta",
+    precio: 80000,
+    imagen: "img producto/organizador.jpg",
+    categoria: "equipaje"
+  },
+  {
+    id: 4,
+    nombre: "Camara descartable",
+    precio: 60000,
+    imagen: "img producto/camara.jpg",
+    categoria: "viajes"
+  }
+];
